a1c27bfa83067d0f6b294adeeaaa07940d157b91
Voinea <voinea.dragos.alexandru@gmail.com>
2019-05-20 12:21:41 +0300

    Revert "MK25SS fix homing X"
    This reverts commit 3f704b4be474bf6ddc6b64606863a36dc4511aa6.

--- Firmware/tmc2130.cpp
+++ Firmware/tmc2130.cpp
@@ -38,12 +38,7 @@ uint8_t tmc2130_mres[4] = {0, 0, 0, 0}; //will be filed at begin of init
 
 
 uint8_t tmc2130_sg_thr[4] = {TMC2130_SG_THRS_X, TMC2130_SG_THRS_Y, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E};
-
-#ifdef GHETTO_TMC
-uint8_t tmc2130_sg_thr_home[4] = {5, 3, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E};
-#else
 uint8_t tmc2130_sg_thr_home[4] = {3, 3, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E};
-#endif
 
 
 uint8_t tmc2130_sg_homing_axes_mask = 0x00;
