956e5482036d0e1d04a4ae107788ad8df50ca58c
Voinea <voinea.dragos.alexandru@gmail.com>
2019-05-31 12:47:04 +0300

    MKP 250000 baud rate

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -68,6 +68,11 @@
 // This determines the communication speed of the printer
 #define BAUDRATE 115200
 
+#ifdef MKP
+#undef BAUDRATE
+#define BAUDRATE 250000
+#endif
+
 // This enables the serial port associated to the Bluetooth interface
 //#define BTENABLED              // Enable BT interface on AT90USB devices
 
