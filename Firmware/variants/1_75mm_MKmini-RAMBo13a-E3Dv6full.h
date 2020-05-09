f7ade42f9dd2f48f6bc98c7d6214f99c9335a728
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2020-05-09 19:35:13 +0300

    Parameter update

--- Firmware/variants/1_75mm_MKmini-RAMBo13a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MKmini-RAMBo13a-E3Dv6full.h
@@ -262,15 +262,15 @@
 #define TMC2130_TCOOLTHRS_E 500       // TCOOLTHRS - coolstep treshold
 
 #define TMC2130_SG_HOMING       1     // stallguard homing
-#define TMC2130_SG_THRS_X       5     // stallguard sensitivity for X axis
-#define TMC2130_SG_THRS_Y       6     // stallguard sensitivity for Y axis
-#define TMC2130_SG_THRS_Z       6     // stallguard sensitivity for Z axis
-#define TMC2130_SG_THRS_E       3     // stallguard sensitivity for E axis
-#define TMC2130_SG_THRS_HOME {125, 3, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E}
+#define TMC2130_SG_THRS_X       0     // stallguard sensitivity for X axis
+#define TMC2130_SG_THRS_Y       0     // stallguard sensitivity for Y axis
+#define TMC2130_SG_THRS_Z     130     // stallguard sensitivity for Z axis
+#define TMC2130_SG_THRS_E       0     // stallguard sensitivity for E axis
+#define TMC2130_SG_THRS_HOME {125, 125, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E}
 
 //new settings is possible for vsense = 1, running current value > 31 set vsense to zero and shift both currents by 1 bit right (Z axis only)
-#define TMC2130_CURRENTS_H {10, 30, 35, 30}  // default holding currents for all axes
-#define TMC2130_CURRENTS_R {16, 30, 35, 30}  // default running currents for all axes
+#define TMC2130_CURRENTS_H {8, 30, 35, 30}  // default holding currents for all axes
+#define TMC2130_CURRENTS_R {10, 30, 35, 30}  // default running currents for all axes
 #define TMC2130_CURRENTS_R_HOME {16, 30, 20, 18}  // homing running currents for all axes
 // #define TMC2130_UNLOAD_CURRENT_R 12			 // lower current for M600 to protect filament sensor - Unused
 
